61fdd4f6d0ae924080b9a6633564d196b1d672c8
jaFuart <kl.yz.vw1337@gmail.com>
2021-12-18 00:35:51 +0300

    load data, make some preparations

---
+++ plots_maker.R
@@ -0,0 +1,23 @@
+
+# Loading data and making some preparations
+currdir <- "./data"
+if(!dir.exists("./data")) dir.create("./data")
+setwd(currdir)
+
+dburl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zip <- "household power consumption.zip"
+download.file(dburl, zip)
+
+if(file.exists(zip)) unzip(zip)
+
+data <- read.csv("household_power_consumption.txt", header=TRUE, sep=';',
+                 nrows=2075259, na.strings="?", check.names=F, 
+                 stringsAsFactors=FALSE, comment.char="", quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+# Subset the data
+sub_data <- subset(data, Date >= "2007-02-01" & Date <= "2007-02-02")
+
+# Converting date formats
+datetime <- paste(as.Date(sub_data$Date), sub_data$Time)
+sub_data$Datetime <- as.POSIXct(datetime)
